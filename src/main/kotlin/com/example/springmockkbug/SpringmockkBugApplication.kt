29f2a3b5129f8f02c501a0289412ac7d21526715
Wojciech Okoński <wojteko22.96@gmail.com>
2020-04-13 12:44:57 +0200

    Init

---
+++ src/main/kotlin/com/example/springmockkbug/SpringmockkBugApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springmockkbug
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringmockkBugApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringmockkBugApplication>(*args)
+}
